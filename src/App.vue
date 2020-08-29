ee29f5f55256672200d7ec4a674bd080214ba548
yuanquanke <3318615021@qq.com>
2020-08-29 09:30:16 +0800

    代码检查

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <!-- <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> -->
+      <router-link to="/about">About</router-link> -->阿斯顿
     </div>
     <router-view/>
   </div>
--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
+
     // {
     //   path: '/',
     //   name: 'home',
